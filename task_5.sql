a26de18e050569f3f7c50267cb2e4cf8bc8a40f3
Thubelihle Ngcobo <thubelihlemd@gmail.com>
2025-06-28 23:47:53 +0200

    task_5.sql

--- task_5.sql
+++ task_5.sql
@@ -56,7 +56,7 @@ CREATE TABLE IF NOT EXISTS Order_Details (
 );
 """")
 
-sql = "INSERT INTO Customer (customer_id, customer_name, email, address ) VALUES (%s, %s, %s, %s)"
+sql = "INSERT INTO customer (customer_id, customer_name, email, address ) VALUES (%s, %s, %s, %s)"
 val = (1, "Cole Baidoo", "cbaidoo@sandtech.com", "123 Happiness Ave.")
 mycursor.execute(sql, val)
 mydb.commit() 
