a3f77b30ac86d21aa6b0e1c8bb05b7e71481680b
Thubelihle Ngcobo <thubelihlemd@gmail.com>
2025-06-29 14:02:38 +0200

    task_6.sql

--- task_6.sql
+++ task_6.sql
@@ -60,7 +60,7 @@ sql = "INSERT INTO customer (customer_id, customer_name, email, address ) VALUES
 val = [
     (2, "Blessing Malik", "bmalik@sandtech.com", "124 Happiness  Ave."),
     (3, "Obed Ehoneah", "eobed@sandtech.com", "125 Happiness  Ave."),
-    (4, "Nehemial Kamolu", "nkamolu@sandtech.co", "126 Happiness  Ave.")
+    (4, "Nehemial Kamolu", "nkamolu@sandtech.com", "126 Happiness  Ave.")
 ]
 
 mycursor.executemany(sql, val)
